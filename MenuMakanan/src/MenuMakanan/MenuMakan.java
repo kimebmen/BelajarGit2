c85397ead79eabb01a32e6ed095afe5009bb063a
kimebmen <simbolonkimebmen@gmail.com>
2017-02-27 18:18:49 +0800

    Blajar upload

---
+++ MenuMakanan/src/MenuMakanan/MenuMakan.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MenuMakanan;
+
+/**
+ *
+ * @author Simbolon-PC
+ */
+public class MenuMakan {
+    public static void main(String[] args) {
+    java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MenuMakanan().setVisible(true);
+            }
+        });
+    }
+}
